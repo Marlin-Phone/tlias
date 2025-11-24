8cb4cbbd015cf24c69687e5607634b8b4691cebf
Marlin <Marlin_Phone@outlook.com>
2025-11-24 23:58:44 +0800

    feat: 实现事务管理员工操作日志功能，记录新增员工操作

--- tlias-web-management/src/main/java/org/example/service/impl/EmpServiceImpl.java
+++ tlias-web-management/src/main/java/org/example/service/impl/EmpServiceImpl.java
@@ -3,14 +3,14 @@ package org.example.service.impl;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import org.example.mapper.EmpExprMapper;
+import org.example.mapper.EmpLogMapper;
 import org.example.mapper.EmpMapper;
-import org.example.pojo.Emp;
-import org.example.pojo.EmpExpr;
-import org.example.pojo.EmpQueryParam;
-import org.example.pojo.PageResult;
+import org.example.pojo.*;
+import org.example.service.EmpLogService;
 import org.example.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDateTime;
@@ -23,6 +23,8 @@ public class EmpServiceImpl implements EmpService {
     private EmpMapper empMapper;
     @Autowired
     private EmpExprMapper empExprMapper;
+    @Autowired
+    private EmpLogService empLogService;
 
     /**
      * PageHelper分页查询实现
@@ -42,23 +44,34 @@ public class EmpServiceImpl implements EmpService {
         return new PageResult<Emp>(p.getTotal(), p.getResult());
     }
 
+//    @Transactional // 事务管理注解 - 默认出现运行时异常(RunTimeException)时才回滚
+    @Transactional(rollbackFor = Exception.class) // 出现任意异常时都回滚
     public void save(Emp emp) {
-        // 1. 保存员工的基本信息
-        emp.setCreateTime(LocalDateTime.now());
-        emp.setUpdateTime(LocalDateTime.now());
-        if (emp.getPhone() == null) {
-            emp.setPhone("");
-        }
-        empMapper.insert(emp);
-        // 2. 保存员工的工作经历信息
-        List<EmpExpr> exprList = emp.getExprList();
-        if(!CollectionUtils.isEmpty(exprList)){
-            // 遍历工作经历列表，设置员工ID
-            for(EmpExpr expr : exprList){
-                expr.setEmpId(emp.getId());
+        try {
+            // 1. 保存员工的基本信息
+            emp.setCreateTime(LocalDateTime.now());
+            emp.setUpdateTime(LocalDateTime.now());
+            if (emp.getPhone() == null) {
+                emp.setPhone("");
             }
-            // 批量插入工作经历数据
-            empExprMapper.insertBatch(exprList);
+            empMapper.insert(emp);
+
+//            int i = 1 / 0; // 制造异常，测试事务回滚
+
+            // 2. 保存员工的工作经历信息
+            List<EmpExpr> exprList = emp.getExprList();
+            if(!CollectionUtils.isEmpty(exprList)){
+                // 遍历工作经历列表，设置员工ID
+                for(EmpExpr expr : exprList){
+                    expr.setEmpId(emp.getId());
+                }
+                // 批量插入工作经历数据
+                empExprMapper.insertBatch(exprList);
+            }
+        } finally {
+            // 记录操作日志
+            EmpLog empLog = new EmpLog(null, LocalDateTime.now(), "新增员工" + emp);
+            empLogService.insertLog(empLog);
         }
     }
 }
