7382095bf6c307cd3a464f06a3a3d93e58827f81
Marlin <Marlin_Phone@outlook.com>
2025-12-06 00:45:55 +0800

    update: 更新动态查询学生信息功能，支持根据姓名、学位和班级ID过滤学生记录

--- tlias-web-management/src/main/java/org/example/mapper/StudentMapper.java
+++ tlias-web-management/src/main/java/org/example/mapper/StudentMapper.java
@@ -12,10 +12,6 @@ import java.util.List;
 @Mapper
 public interface StudentMapper {
 
-    @Select("select s.*, c.name as clazz_name " +
-            "from student s " +
-            "         left join clazz c on s.clazz_id = c.id " +
-            "order by update_time desc")
     List<Student> list(StudentQueryParam studentQueryParam);
 
     void delete(List<Integer> ids);
